8afe8449dc91735e1c7d439c9c910756ec72dcca
Johan Robles <johanrobles66@yahoo.com>
2023-04-13 20:21:18 -0500

    Adición de la fichas y descripcion del código

--- Ficha/Ficha.gd
+++ Ficha/Ficha.gd
@@ -1,6 +1,12 @@
 extends Sprite
 
+
+# TO DO: lo ideal es hacer un algoritmo en la que se iteré por todos las posibles enemigos de
+# la ficha. Esto para los caso en la que puede comer 2, 3 o mas ficha enemigas a la vez.
+
+
 var salida = false
+var posicion_inicial = Vector2(65, 546)
 
 func _ready():
 	pass
@@ -10,21 +16,27 @@ func salio_de_casa():
 var posicion_actual = 1	
 
 
-func mover_ficha(dado1_valor, dado2_valor, position_ficha, casilla_nodo):
+func mover_ficha(dado1_valor, dado2_valor, position_ficha):
 	# Si los dados tienen el mismo valor, mueve la ficha
+	# Esto aplica cuando una ficha esta en la casa, para sacarla 
+	
+	var tween = position_ficha.get_node("MovimientoC11")
 	if dado1_valor == dado2_valor and !(salida):
 		# Obtiene la posición final de la casilla correspondiente
-		var posicion_final = casilla_nodo.get_global_position()
+		var posicion_final = get_node("../../Casilla1").get_global_position()
 		
 		# Crea una interpolación Tween para mover la ficha
-		var tween = position_ficha.get_node("MovimientoC11")
+		#var tween = position_ficha.get_node("MovimientoC11")
 		tween.interpolate_property(position_ficha, "position", position_ficha.position, posicion_final, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 		tween.start()
 		yield(tween, "tween_completed")
+		# Cambiamos el valor de salida dado que ya la ficha salio de la casa y ademas verificamos
+		# si esta come
 		salio_de_casa()
 		comer_ficha(position_ficha.position, 2)
 	elif salida:
-		
+		# Si la ficha ya esta afuera de la casa, entonces se mueve por la casillas
+		# de acuerdo al numero de un dado o dos (por ahora los dos)
 		var posicion_final = posicion_actual + dado1_valor + dado2_valor + 2
 
 		# Mueve la ficha en cada casilla una por una hasta llegar a la casilla final
@@ -34,7 +46,7 @@ func mover_ficha(dado1_valor, dado2_valor, position_ficha, casilla_nodo):
 			var siguiente_casilla = get_node("../../Casilla"+str(i))
 
 		# Crea una interpolación Tween para mover la ficha a la siguiente casilla
-			var tween = get_node("../MovimientoC11")
+			#var tween = get_node("../MovimientoC11")
 			tween.interpolate_property(position_ficha, "position", casilla_actual.get_global_position(), siguiente_casilla.get_global_position(), 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 			tween.start()
 
@@ -43,11 +55,14 @@ func mover_ficha(dado1_valor, dado2_valor, position_ficha, casilla_nodo):
 
 			# Espera a que la interpolación Tween termine antes de continuar
 			yield(tween, "tween_completed")
-		
+		# Luego de llegar a su ultima casilla correspondiente vemos si pude comer a otra ficha
 		comer_ficha(position_ficha.position, 1)
 
 func comer_ficha(posicion_final, i):
+	# obtenemos el nodo de la ficha enemiga
 	var ficha_enemiga = get_node("../../FichaC2" + str(i))
+	# Si la posicion  del puntero de la ficha enemiga es igual a a la posicion de la ficha
+	# entonces la ficha enemiga retorna a su casa a su posicion inicial
 	if ficha_enemiga.global_position == posicion_final:
 		# Hacer desaparecer la ficha enemiga
 		#ficha_enemiga.queue_free()
@@ -57,4 +72,3 @@ func comer_ficha(posicion_final, i):
 		tween.interpolate_property(ficha_enemiga, "position", ficha_enemiga.position, casa_enemiga.position, 0.25, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 		tween.start()
 		yield(tween, "tween_completed")
-
--- Tablero/Tablero.gd
+++ Tablero/Tablero.gd
@@ -1,16 +1,29 @@
 extends Node2D
 
+# TO DO: Hacer la gestion de turnos de cada jugar (de hecho, según los diagramas que tenemos, deberia de existir
+	#   un game master para esto, lo ideal crear un script aparte o global)
 
+# TO DO: Hacer la funcionalida de "soplar" (lo mismo, lo ideal seria introducirlo en el game master)
+
+# TO DO: el jugador con el turno correspondiente debera seleccionar algunas de las fichas que tiene 
+# disponibles en el campo de juego (digo "algunas" ya que el jugador tendra la posibilidad de mover
+# una ficha de acuerdo al valor de un dado y otra ficha de acuerdo al valor del otro, o bien, mover 
+# la ficha de acuerdo al valor de los dos dados)
+
+# TO DO: se muestre los label correspondientes que indiquen informacion necesaria para los jugadores como
+# turno del jugador correspondiente, tiempo, funcionalidad de lanzar los dados, botones
+# o funcionalidad de hacer que la ficha seleccionada se mueva de acuerdo a tres casos:
+		# 1. mover n casillas de acuerdo al valor del dado1
+		# 2. mover n casillas de acuerdo al valor del dado2
+		# 3. mover n casilla de acuerdo al valor de la suma de los dados
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
 func _on_Button_pressed():
 	lanzar_dados()
 	
@@ -22,9 +35,8 @@ func lanzar_dados():
 	# Seleccionar un frame aleatorio para cada dado
 	var frame_dado1 = round(rand_range(0, 5))
 	var frame_dado2 = round(rand_range(0, 5))
-	print(frame_dado1, frame_dado2)
 	# Establecer el frame seleccionado para cada dado
 	dado1.frame = frame_dado1
 	dado2.frame = frame_dado2
 	
-	$FichaC11/ficha.mover_ficha(frame_dado1, frame_dado2, $FichaC11, get_node("Casilla1"))
+	$FichaC11/ficha.mover_ficha(frame_dado1, frame_dado2, $FichaC11)
